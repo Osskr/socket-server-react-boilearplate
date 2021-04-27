12ab44ea330fcebcbf8cbd3488d9f0ab967dab3b
osskr <osskrjuarez@gmail.com>
2021-04-27 11:41:26 -0300

    renamed file Server.js>server.js

--- models/Server.js
+++ models/Server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const socketio = require('socket.io')
-const cors = require('cors');
+
 const path = require('path');
 const Sockets = require('../controllers/sockets.controller');
 
