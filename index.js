d06efbf0582dfd59bdddbec0ea4ff0707fa0d99d
osskr <osskrjuarez@gmail.com>
2021-04-27 09:36:19 -0300

    setting-up server

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const Server = require('./models/server')
+
+
+
+//Server 
+ const server = new Server()
+ server.listen()
+ 
---
+++ models/Server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const socketio = require('socket.io')
+const cors = require('cors');
+const path = require('path');
+const Sockets = require('../controllers/sockets.controller');
+
+class Server{
+    
+    constructor(){
+            
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Http Server config
+        this.server = require('http').createServer(this.app)
+        
+        //Socketio config
+        this.io = socketio(this.server, {/*configuraciones */})
+
+
+        this.paths = {}
+
+        //Middlewares
+        this.middlewares();
+        //Rutas de mi aplicacion
+        this.routes();
+        //Sockets
+        this.sockets();
+    }
+
+    middlewares(){
+
+        // Desplegar el directorio publico
+        this.app.use(express.static(path.resolve(__dirname ,'../public')))
+ 
+    }
+
+    routes(){
+
+    }
+
+    // Configuracion de Sockets
+    sockets(){
+     new Sockets(this.io);
+    }
+
+    listen(){
+       
+        
+        this.server.listen(this.port, ()=>{
+            console.log('Server running at: ',this.port)
+        })
+    }
+}
+
+module.exports = Server;
